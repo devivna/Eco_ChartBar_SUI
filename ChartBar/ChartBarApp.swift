f78d0078b86acfe609c960345d5391d5521e14a2
Nadia <ivashchenko.nadia@gmail.com>
2023-02-07 21:06:35 +0200

    Initial Commit

---
+++ ChartBar/ChartBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartBarApp.swift
+//  ChartBar
+//
+//  Created by Students on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChartBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
